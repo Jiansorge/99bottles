c9f52eb6d1c363938d5e6d13639abdc909b6ac78
[jiansorge] <[ji@nsorge.com]>
2018-05-23 23:34:08 -0700

    first commit

---
+++ 99bottles.js
@@ -0,0 +1,30 @@
+var kombuchaCount = 100;
+//while (kombuchaCount > 0){
+//  console.log(kombuchaCount + " bottles of kombucha on the wall. " + kombuchaCount + " bottles of kombucha.");
+//  console.log("Take one down, pass it around, ");
+//  kombuchaCount--;
+//  console.log(kombuchaCount + " bottles of kombucha on the wall!");
+//}
+//for (var komuchaCount = 100; kombuchaCount > 100; kombuchaCount--){
+//    console.log(kombuchaCount + " bottles of kombucha on the wall. " + kombuchaCount + " bottles of kombucha.");
+//  console.log("Take one down, pass it around, ");
+//  kombuchaCount--;
+//  console.log(kombuchaCount + " bottles of kombucha on the wall!");
+//}
+var kombuchaCount = 100;
+ while (kombuchaCount > 0) {
+     console.log(kombuchaCount + " bottles of kombucha on the wall, " + kombuchaCount + " bottles of kombucha");
+     console.log("take one down, pass it around");
+     // kombuchaCount = kombuchaCount = 1;
+     // kombuchaCount--;
+     kombuchaCount -= 1;
+     console.log(kombuchaCount + " bottles of kombucha on the wall.");
+ }
+for (var kombuchCount = 100; kombuchaCount > 100; kombuchaCount--) {
+//     console.log(kombuchaCount + " bottles of kombucha on the wall, " + kombuchaCount + " bottles of kombucha");
+//     console.log("take one down, pass it around");
+//     // kombuchaCount = kombuchaCount = 1;
+//     // kombuchaCount--;
+//     kombuchaCount -= 1;
+//     console.log(kombuchaCount + " bottles of kombucha on the wall.");
+}
